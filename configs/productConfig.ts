c08def9fe70b996938fecf2354d1f4ff7dd479a8
Preeyawat Inhangwa <preeyawat@jibsoft.co.th>
2023-04-24 10:15:05 +0700

    start project

---
+++ configs/productConfig.ts
@@ -0,0 +1,21 @@
+interface ProductConfigInterface {
+  id: number;
+  type: string;
+  name: string | null;
+  description: string | null;
+  quantity: number;
+  price: number;
+  updateDate: string;
+}
+
+// export const productConfigs: ProductConfigInterface[] = [
+//     {
+//         id: 1,
+//         type: string;
+//         name: string | null;
+//         description: string | null;
+//         quantity: number;
+//         price: number;
+//         updateDate: string;
+//     }
+// ];
